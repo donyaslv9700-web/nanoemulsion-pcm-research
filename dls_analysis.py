47a6a729f72e458dc0a7268a39b72d0125f9a9b3
donya <donyaslv9700@gmail.com>
2026-06-27 15:18:43 +0330

    Update dls_analysis.py
    This Python script performs a basic simulation of Dynamic Light Scattering (DLS) particle size distribution. It plots particle size versus intensity to visualize size distribution trends in colloidal systems.

--- dls_analysis.py
+++ dls_analysis.py
@@ -1,1 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+# Sample DLS particle size data (nm)
+particle_size = np.array([10, 20, 30, 40, 50, 60])
+intensity = np.array([5, 15, 40, 25, 10, 5])
+
+plt.figure()
+plt.plot(particle_size, intensity, marker='o')
+
+plt.title("DLS Particle Size Distribution")
+plt.xlabel("Particle Size (nm)")
+plt.ylabel("Intensity (%)")
+
+plt.grid(True)
+plt.show()
